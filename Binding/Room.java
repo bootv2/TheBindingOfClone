0064b3ae04b506f19bd84260bed25467bf837903
Tim Boode <tim@compuboot.in>
2014-10-07 09:21:02 +0200

    gui selection and project start

---
+++ Binding/Room.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Room here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Room extends World
+{
+
+    /**
+     * Constructor for objects of class Room.
+     * 
+     */
+    public Room()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1250, 674, 1); 
+    }
+}
