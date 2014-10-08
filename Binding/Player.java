b2fd341d4f41e92585aeec597943286f00f6d436
Hans <kurniaw.hans@gmail.com>
2014-10-08 09:02:46 +0200

    player anim and movement

--- Binding/Player.java
+++ Binding/Player.java
@@ -14,6 +14,85 @@ public class Player extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+    if(Greenfoot.isKeyDown("right")){
+            moveRight();
+            animRight();
+    }
+    if(Greenfoot.isKeyDown("left")){
+            moveLeft();
+            animLeft();
+    }
+    if(Greenfoot.isKeyDown("up")){
+            moveUp();
+            animUp();
+    }
+    if(Greenfoot.isKeyDown("down")){
+            moveDown();
+            animDown();
+    }   
+}
+private void animRight()
+    {
+        if(frameRight == 0) {setImage("pWalkRight0.png");}
+        else if(frameRight == 4) {setImage("pWalkRight1.png");}
+        else if(frameRight == 8)
+        {
+            frameRight = 0;
+            return;
+        }
+        frameRight++;
+    }
+    private void animLeft()
+    {
+        if(frameLeft == 0) {setImage("pWalkLeft0.png");}
+        else if(frameLeft == 4) {setImage("pWalkLeft1.png");}
+        else if(frameLeft == 8)
+        {
+            frameLeft = 0;
+            return;
+        }
+        frameLeft++;
+    }
+        private void animUp()
+    {
+        if(frameUp == 0) {setImage("pWalkUp0.png");}
+        else if(frameUp == 4) {setImage("pWalkUp1.png");}
+        else if(frameUp == 8)
+        {
+            frameUp = 0;
+            return;
+        }
+        frameUp++;
+    }
+        private void animDown()
+    {
+        if(frameDown == 0) {setImage("pWalkDown0.png");}
+        else if(frameDown == 4) {setImage("pWalkDown1.png");}
+        else if(frameDown == 8)
+        {
+            frameDown = 0;
+            return;
+        }
+        frameDown++;
+    }
+    private void moveLeft()
+    {
+        setLocation(getX() - 5, getY());
+    }
+        private void moveRight()
+    {
+        setLocation(getX() + 5, getY());
+    }
+        private void moveUp()
+    {
+        setLocation(getX() , getY() - 5);
+    }
+        private void moveDown()
+    {
+        setLocation(getX() , getY() + 5);
+    }
+private int frameRight;
+private int frameLeft;
+private int frameUp;
+private int frameDown;
 }
