74303b944748f677882bc2333039da993596f845
Tim Boode <tim@compuboot.in>
2014-10-08 11:05:45 +0200

    map generation finished, TODO room generation

--- Binding/Player.java
+++ Binding/Player.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Player extends Actor
+public class Player extends Mover
 {
     /**
      * Act - do whatever the Pickup wants to do. This method is called whenever
